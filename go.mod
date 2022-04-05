e026544857f45f5f3aeb7818fda02dfd92ef6338
pierreEnoc <pierre_enoc@yahoo.fr>
2022-04-05 16:03:42 -0300

    Open close db

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/pierreEnoc/goflix-fr
 
 go 1.18
+
+require (
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/mattn/go-sqlite3 v1.14.12
+)
---
+++ store.go
@@ -0,0 +1,36 @@
+package main
+
+// sqlx
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+)
+
+type Store interface {
+	Open() error
+	Close() error
+}
+
+// DB
+type dbStore struct {
+	db *sqlx.DB
+}
+
+// implement interface
+func (store *dbStore) Open() error {
+	db, err := sqlx.Connect("sqlite3", "goflix.db")
+	if err != nil {
+		return err
+	}
+
+	log.Println("Connected to DB")
+	store.db = db
+	return nil
+}
+
+func (store *dbStore) Close() error {
+	return store.db.Close()
+}
