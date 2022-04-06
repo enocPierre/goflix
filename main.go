c85d762e0168e4cefbe019450a95ae196ebd0df8
pierreEnoc <pierre_enoc@yahoo.fr>
2022-04-05 21:22:44 -0300

    refactor main

--- main.go
+++ main.go
@@ -8,13 +8,22 @@ import (
 func main() {
 	fmt.Println("Goflix")
 
+	if err := run(); err != nil {
+		if  err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func run() error {
 	srv := newServer()
 	srv.store = &dbStore{}
 	err := srv.store.Open()
 	if  err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
+		return err
 	}
 
 	defer srv.store.Close()
+	return nil
 }
