edd6a560a0d91ba29eb18b5c7123e8923f8c5ee2
pierreEnoc <pierre_enoc@yahoo.fr>
2022-04-05 21:12:14 -0300

    add sqlite3 bd

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Goflix")
+
+	srv := newServer()
+	srv.store = &dbStore{}
+	err := srv.store.Open()
+	if  err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+
+	defer srv.store.Close()
 }
