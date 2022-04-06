748aebcfaf28a2e0bd1e8886486c00559dd852ba
pierreEnoc <pierre_enoc@yahoo.fr>
2022-04-05 22:15:14 -0300

    add sqlite

--- store.go
+++ store.go
@@ -19,6 +19,17 @@ type dbStore struct {
 	db *sqlx.DB
 }
 
+var schema = `
+CREATE TABLE IF NOT EXISTS movie
+(
+	id INTEGER PRIMARy KEY AUTOINCREMENT,
+	title TEXT,
+	release_date TEXT,
+	duration INTEGER,
+	trailer_url TEXT
+)
+`
+
 // implement interface
 func (store *dbStore) Open() error {
 	db, err := sqlx.Connect("sqlite3", "goflix.db")
@@ -27,6 +38,7 @@ func (store *dbStore) Open() error {
 	}
 
 	log.Println("Connected to DB")
+	db.MustExec(schema)
 	store.db = db
 	return nil
 }
